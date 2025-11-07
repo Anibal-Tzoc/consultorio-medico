156d6eb7eb1731db7897ba7024f3af3454285746
Anibal-Tzoc <anitzoc10@gmail.com>
2025-11-06 19:52:31 -0600

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,9 @@
 session_start(); // Inicia sesiones para auth
 
 // Define variables de entorno (de Railway o local)
-$DB_HOST = getenv('DB_HOST') ?: '127.0.0.1'; // mysql.railway.internal en Railway
+$DB_HOST = getenv('DB_HOST') ?: 'mysql.railway.internal'; // mysql.railway.internal en Railway
 $DB_NAME = getenv('DB_NAME') ?: 'railway'; // o 'consultorio_medico' local
-$DB_PASSWORD = getenv('DB_PASSWORD') ?: 'hVQXbZykIwasSgczFFymXvrUzmqwzqRF
-
-
-
-'; // tu pass: hVQXbZykIwasSgczFFymXvrUzmqwzqRF
+$DB_PASSWORD = getenv('DB_PASSWORD') ?: 'hVQXbZykIwasSgczFFymXvrUzmqwzqRF'; // tu pass: hVQXbZykIwasSgczFFymXvrUzmqwzqRF
 $DB_PORT = getenv('DB_PORT') ?: '3306';
 $DB_USER = getenv('DB_USER') ?: 'root';
 
@@ -72,4 +68,5 @@ if (!file_exists($upload_dir)) {
     mkdir($upload_dir, 0755, true); 
 }
 ?>
+
 
